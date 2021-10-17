553b80ff6760a0365624d2c94d7748251233bf3a
Johnny Westlake <jmdwestlake@yahoo.co.uk>
2021-10-17 18:03:59 +0100

    SDK Bug

---
+++ FontDeath/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace FontDeath
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            if (Window.Current.Content is null)
+                Window.Current.Content = new MainPage();
+
+            Window.Current.Activate();
+        }
+    }
+}
